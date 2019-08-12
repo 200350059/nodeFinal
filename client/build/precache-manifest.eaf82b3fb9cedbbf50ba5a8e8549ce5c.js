634b91ae007ebbab08c183d67ef214e91e503ed2
Karankumar Patel <200350059@student.georgianc.on.ca>
2019-08-11 21:08:54 -0400

    trouble shooting

---
+++ client/build/precache-manifest.eaf82b3fb9cedbbf50ba5a8e8549ce5c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a57eb6f99b6ff91747bdeaf9b7c255b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "061e6f9ccece839f8c37",
+    "url": "/static/js/2.c68f7615.chunk.js"
+  },
+  {
+    "revision": "56739596e1883a15228d",
+    "url": "/static/js/main.1d4c7b4d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
